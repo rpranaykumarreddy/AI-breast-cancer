5a3de0e49d03e6bc1cb8b8bf69b9ad7793eef68d
Ravula Pranay Kumar Reddy <72241752+rpranaykumarreddy@users.noreply.github.com>
2022-02-19 12:35:24 +0530

    Update settings.py

--- final/settings.py
+++ final/settings.py
@@ -113,9 +113,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
-]
 STATIC_ROOT = os.path.join(BASE_DIR, 'files')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
